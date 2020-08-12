563e8f89a7937ce6cc7a80bbbd1bc8ffe07b57e0
Robert-Siberry <robert.siberry@outlook.com>
2020-08-12 08:52:24 +0000

    removed service 2 from service 1 folder

--- Service_2/application/routes.py
+++
@@ -1,9 +0,0 @@
-from application import app
-from flask import render_template, request
-import requests
-import random
-
-@app.route('/race', methods=['GET'])
-def race():
-    list = ['Dragonborn','Dwarf','Elf','Gnome','Half-Elf','Halfling','Half-Orc','Human','Tiefling']
-    return list[random.randrange(8)]
