c0ea9ec8bf7588421198065844292a9c92c96509
Robert-Siberry <robert.siberry@outlook.com>
2020-08-12 09:47:52 +0000

    removed service 1 3 and 4 from service 2 branch

--- Service_3/application/routes.py
+++
@@ -1,9 +0,0 @@
-from application import app
-from flask import render_template, request
-import requests
-import random
-
-@app.route('/dndclasses', methods=['GET'])
-def dndclasses():
-    list = ['Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard']
-    return list[random.randrange(11)]
