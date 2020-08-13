0a71fbe15908da06cb72e2ba42c9f5239818ad0d
Robert-Siberry <robert.siberry@outlook.com>
2020-08-13 17:39:53 +0000

    updated scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker service update --image robertsiberry/service_1:latest service_1
-docker service update --image robertsiberry/service_2:latest service_2
-docker service update --image robertsiberry/service_3:latest service_3
-docker service update --image robertsiberry/service_4:latest service_4
+docker service update --image robertsiberry/service_1:latest fopnpservice_service_1
+docker service update --image robertsiberry/service_2:latest fopnpservice_service_2
+docker service update --image robertsiberry/service_3:latest fopnpservice_service_3
+docker service update --image robertsiberry/service_4:latest fopnpservice_service_4
 docker rmi $(docker images -f "dangling=true" -q) -f
 
 docker system prune -f
