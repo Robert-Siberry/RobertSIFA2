55501cce7ac0bbb22df3ac1ed76d67d325347491
Robert-Siberry <robert.siberry@outlook.com>
2020-08-16 17:12:31 +0000

    removed clean script for testing

--- scripts/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker service update --image robertsiberry/service_1:latest fopnpservice_service_1
-docker service update --image robertsiberry/service_2:latest fopnpservice_service_2
-docker service update --image robertsiberry/service_3:latest fopnpservice_service_3
-docker service update --image robertsiberry/service_4:latest fopnpservice_service_4
-docker rmi $(docker images -f "dangling=true" -q) -f
-
-docker system prune -f
